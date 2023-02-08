e1ec8884cf94498c30164ecedc71a2cf9d318244
Dominik Lindner <d.lindner@dundee.ac.uk>
2023-02-08 20:05:33 +0000

    Also add ROIs to MIP images

--- scripts/rois.py
+++ scripts/rois.py
@@ -64,11 +64,11 @@ def getImage(plate, row, col, field):
 def process_file(file, conn):
     root = ET.parse(file).getroot()
     plate_name = root.findtext("n:PlateName", namespaces=NS)
-
-    plate_name = re.sub(r"_Day.+$", "", plate_name)
-    print(f"Plate: {plate_name}")
+    plate_name = re.sub("^Screen_", "", plate_name)
+    mip_name = re.sub(r"_Day.+$", "_MIP", plate_name)
+    print(f"Processing Plates: {plate_name} and {mip_name}")
     plate = conn.getObject('Plate', attributes={'name': plate_name})
-    print(plate)
+    mip = conn.getObject('Plate', attributes={'name': mip_name})
 
     for rt_el in root.findall("n:ResultTable", NS):
         type = rt_el.findtext("n:Name", namespaces=NS)
@@ -78,7 +78,8 @@ def process_file(file, conn):
             field = int(rt_el.get("FieldID"))
             print(f"Row: {row}, Col: {col}, Field: {field}")
             image = getImage(plate, row-1, col-1, field-1)
-            if image:
+            mip_image = getImage(mip, row-1, col-1, field-1)
+            if image or mip_image:
                 for t_el in rt_el.findall("n:table/n:tr", NS):
                     n = t_el.get("n")
                     p_x = int(t_el.get("x"))
@@ -92,15 +93,14 @@ def process_file(file, conn):
                     h = int(bb[3])
                     print(f"Object: {n}, Point: X: {p_x}, Y: {p_y}")
                     print(f"Box: x: {x}, Y: {y}, w: {w}, h: {h}")
-                    roi = create_roi(image, p_x, p_y, x, y, w, h, n)
-                    roi = conn.getUpdateService().saveAndReturnObject(roi, conn.SERVICE_OPTS)
-                    if roi:
-                        print("ROI saved.")
-                    else:
-                        print("ROI saving failed.")
+                    if image:
+                        roi = create_roi(image, p_x, p_y, x, y, w, h, n)
+                        conn.getUpdateService().saveAndReturnObject(roi, conn.SERVICE_OPTS)
+                    if mip_image:
+                        roi = create_roi(mip_image, p_x, p_y, x, y, w, h, n)
+                        conn.getUpdateService().saveAndReturnObject(roi, conn.SERVICE_OPTS)
             else:
                 print("Image not found")
-
 
 
 object_file = sys.argv[1] # "ObjectList_00000169.xml"
